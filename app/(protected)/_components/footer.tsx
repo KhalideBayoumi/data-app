1ec43763209bfbf2a35fe807112beef6f289ce09
KhalideB <khalide.bayoumi.ep@whoz.com>
2024-09-23 12:25:06 +0200

    Padding footer

--- app/(protected)/_components/footer.tsx
+++ app/(protected)/_components/footer.tsx
@@ -7,18 +7,18 @@ export const Footer = () => {
 
   return (
     <div className="border-t z-20 w-full">
-      <div className="mx-4 md:mx-8 flex h-14 items-center justify-between">
-        <Link 
-          href="#"
-        >
-          <div className="bg-blue-600 p-1 rounded-sm">
-            <Linkedin className="w-4 h-4" />
-          </div>
-        </Link>
-        <p className="text-xs md:text-sm text-muted-foreground">
-          © {currentYear} Dataguru
-        </p>
-      </div>
+        <div className="max-w-[1440px] mx-auto flex h-14 items-center justify-between px-4 sm:px-6 md:px-8">
+            <Link 
+            href="#"
+            >
+            <div className="bg-blue-600 p-1 rounded-sm">
+                <Linkedin className="w-4 h-4" />
+            </div>
+            </Link>
+            <p className="text-xs md:text-sm text-muted-foreground">
+            © {currentYear} Dataguru
+            </p>
+        </div>
     </div>
   );
 };
